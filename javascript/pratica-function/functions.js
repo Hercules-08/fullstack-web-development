d9089a9024cd2209238412a6f50547f2995e5970
Hercules-08 <jhamisonsilva@gmail.com>
2026-06-22 20:17:40 +0100

    Atualizando-praticas

---
+++ javascript/pratica-function/functions.js
@@ -0,0 +1,13 @@
+function saudacao(nome) {
+    return 'Olá, ' + nome + '!';
+}
+
+function soma(a, b) {
+    return a + b;
+}
+
+let saudacao = function(nome) {
+    return 'Olá, ' + nome + '!';
+}
+
+let soma = (a, b) => a + b;
