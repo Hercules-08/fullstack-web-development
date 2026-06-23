89f81c65514d11b1c62283113c0f80ef4769ba97
Hercules-08 <jhamisonsilva@gmail.com>
2026-06-23 19:07:14 +0100

    atualizando-praticas

---
+++ javascript/pratica-operador-logico/operadores.js
@@ -0,0 +1,8 @@
+let username = "admin";
+let password = "1234";
+
+    if((username === "admin" || username === "user") && password === "1234") {
+        console.log("Login bem-sucedido");
+    }else {
+        console.log("Login falhado");
+    }
---
+++ javascript/pratica-perimetro-Math.PI/perimetro.js
@@ -0,0 +1,4 @@
+let raio = 7; 
+
+    let perimetro = 2 * Math.PI * raio;
+    console.log(perimetro); 
