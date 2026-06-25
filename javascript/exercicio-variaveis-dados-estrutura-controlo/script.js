c62ada89408bee61f007ffedc6238801da2c2c8d
Hercules-08 <jhamisonsilva@gmail.com>
2026-06-25 20:48:07 +0100

    atualizando

---
+++ javascript/exercicio-variaveis-dados-estrutura-controlo/script.js
@@ -0,0 +1,26 @@
+const nome = "Peter";
+const identificador = 852;
+const verdade = true;
+const vazia = null;
+const nãoDefinida = undefined;
+
+let jogo = "God of War";
+let codigo = 789;
+let falso = false;
+let empty = null;
+let semDefinicao = undefined;
+
+let simbolo1 = Symbol("Uma descrição muito foda.")
+    console.log(typeof simbolo1);
+
+let dados = [
+    "Peter",
+    29,
+    62.5,
+    true,
+    null
+];
+console.log(typeof dados[0]);
+console.log(dados.length);
+
+
