3c31116b0853105a8a3225442d0505aa0eab5aa0
Hercules-08 <jhamisonsilva@gmail.com>
2026-06-26 19:36:18 +0100

    treinando

--- javascript/exercicio-variaveis-dados-estrutura-controlo/script.js
+++ javascript/exercicio-variaveis-dados-estrutura-controlo/script.js
@@ -22,5 +22,76 @@ let dados = [
 ];
 console.log(typeof dados[0]);
 console.log(dados.length);
+for (let i = 0; i < dados.length; i++) {
+    console.log(dados[i]);
+}
 
+let cadastro = {
+    nome:"Peter",
+    idade: 30,
+    ativo: true
+};
 
+console.log(cadastro.nome);
+console.log(cadastro.idade);
+
+let maiorIdade = 18;
+
+if(maiorIdade >= 18) {
+    console.log("Você é maior de idade!");
+}else {
+    console.log("Você é menor de idade!");
+};
+
+let dias = 6;
+let diaSemana;
+switch(dias) {
+    case 1:
+        diaSemana = "Hoje é final de semana";
+        break;
+    case 2:
+        diaSemana = "Segunda-feira";
+        break;
+    case 3:
+        diaSemana = "Terça-feira";
+        break;
+    case 4:
+        diaSemana = "Quarta-feira";
+        break;
+    case 5:
+        diaSemana = "Quinta-feira";
+        break;
+    case 6:
+        diaSemana = "Sextou, Carai";
+        break;
+    case 7:
+        diaSemana = "Hoje é final de semana";
+        break;
+    default:
+        diaSemana = "Dia inválido"
+}
+console.log(diaSemana)
+
+let numero = 1;
+
+while (numero <= 10) {
+    console.log(numero);
+    numero++;
+}
+
+let linguagem = "javascript";
+
+for (let letra of linguagem) {
+    console.log(letra);
+}
+
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5, 5));
+console.log(soma(3, 4));
+console.log(soma(5, 4));
+
+let soma2 = (x, y) => x + y;
+console.log(soma2(4, 4));
+console.log(soma2(2, 23));
